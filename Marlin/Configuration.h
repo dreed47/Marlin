8679cc6cd5b50f801ba898b818f37ab872f2eed4
david reed <212698+dreed47@users.noreply.github.com>
2023-02-26 19:59:51 -0500

    garage updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -7,7 +7,7 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or33333
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -1703,7 +1703,7 @@
 // @section motion
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR true
+#define INVERT_X_DIR false
 #define INVERT_Y_DIR true
 #define INVERT_Z_DIR true
 //#define INVERT_I_DIR false
