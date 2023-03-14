2ba4bb61bfb23cbb1c9bae8e6ac1cc8f820bc043
david reed <212698+dreed47@users.noreply.github.com>
2023-03-14 11:35:09 -0400

    FINALLY got hot end fan to work

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2307,7 +2307,7 @@
 #define PREHEAT_1_TEMP_HOTEND 200
 #define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_TEMP_CHAMBER 35
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED    120 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 220
